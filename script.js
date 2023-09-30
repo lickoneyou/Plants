4c12e016cc80f995f9022eb31b4ca956b73c4de7
KIRYL KHOMCHANKA <dickdurex265@gmail.com>
2023-09-30 15:53:18 +0300

    add blur

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 // popUp
 
-const popUpBtn = document.querySelector('.nav380')
-const popUp = document.querySelector('.popUp')
+const popUpBtn = document.querySelector(".nav380");
+const popUp = document.querySelector(".popUp");
 
-popUpBtn.addEventListener('click', function () {
-  if (this.style.transform === 'rotate(90deg)') {
-    this.style.transform = 'rotate(0deg)'
-    popUp.style.right = 0
-    popUp.style.right = '-100%'
+popUpBtn.addEventListener("click", function () {
+  if (this.style.transform === "rotate(90deg)") {
+    this.style.transform = "rotate(0deg)";
+    popUp.style.right = 0;
+    popUp.style.right = "-100%";
   } else {
-    this.style.transform = 'rotate(90deg)'
-    popUp.style.right = 0
+    this.style.transform = "rotate(90deg)";
+    popUp.style.right = 0;
   }
-})
+});
+
+// blur
+
+const serviceTitleBtns = document.querySelectorAll(
+  ".serviceTitleBtns > button"
+);
+const projects = document.querySelectorAll(".projects > div");
+
+serviceTitleBtns.forEach((el) =>
+  el.addEventListener("click", function () {
+    serviceTitleBtns.forEach((e) => e.classList.remove("active"));
+    el.classList.toggle("active");
+    projects.forEach((elem) =>
+      elem.dataset.title == el.innerText && el.classList.contains("active")
+        ? (elem.style.filter = "blur(3px)")
+        : (elem.style.filter = "blur(0px)")
+    );
+  })
+);
